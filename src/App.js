3104dcfb62a0ea641c4050ace5f15f1fe357c1be
shehanblast <shihanbinuka@gmail.com>
2021-05-07 10:50:05 +0530

    Add Class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Reason why we use class is it contains more functionality than function.
+//As a result we have the access to the render method
+//Another benifit is we can access to the state(change object when its clicked)
+class App extends Component{
+    constructor() {
+        super();
+        this.state = {
+            string : "Hello Binuka"
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        {this.state.string}
+                    </p>
+                    {/*Change the State name on Button click*/}
+                    <button onClick={() => this.setState({string:"Bye Binuka"})}>Change Text</button>
+                </header>
+            </div>
+        )
+    }
 }
+
+////////////////////////////////////////////////////////////
+//Same Thing with the class component
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           My name is Binuka.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
 
 export default App;
