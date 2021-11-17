81fe46c22dace0cb6a5cbe66d83723c6f09173cf
shehanblast <shihanbinuka@gmail.com>
2021-11-17 09:53:59 +0530

    Final push

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {Component} from "react";
 import {CardList} from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component";
 
 //Reason why we use class is it contains more functionality than function.
 //As a result we have the access to the render method
@@ -11,6 +12,7 @@ class App extends Component{
         super();
         this.state = {
             monsters : [],
+            search : '',
             string : 'Hello Binuka',
             abb : [
                 {
@@ -27,6 +29,8 @@ class App extends Component{
                 }
             ]
         }
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
     //when this component mounts it calls this method
@@ -37,14 +41,35 @@ class App extends Component{
             .then(users => this.setState({monsters : users}));
     }
 
+    handleChange(e) {
+        this.setState({search : e.target.value})
+    }
+
+    // binding
+    // if we use arroe function we dont have to bind it will refer to the App
+    // 1) this.handleClisk() => dont call the function because it will execute when the page loads/ not when the button clicks
+    // 2) handlechange = () => this way we dont need to bind the value
+    // 3) 3rd way is implemented in the code
+
     render() {
+        const searchField = this.state.search;
+        const monsters = this.state.monsters;
+        const filteredMonstors = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
         return (
             <div className="App">
 
-                {/*Pass monster array to the props*/}
-                <CardList monsters={this.state.monsters}>
-                    {/*Pass as props.children*/}
+                <h1>Monsters Rolodex</h1>
 
+                <SearchBox
+                    placeholder="Search Monster"
+                    handleChange={this.handleChange}
+                />
+
+                {/*Pass monster array to the props*/}
+                <CardList monsters={filteredMonstors}>
+                    {/*Pass as props.children*/}
                 </CardList>
 
                 {/*Access array from map*/}
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import "./card.styles.css"
 export const Card = props => {
     return <div className="card-container">
         <img src={`https://robohash.org/${props.monsters.id}?set=set2&size=180x180`} alt="monster"/>
-        <h1> {props.monsters.name} </h1>
+        <h2> {props.monsters.name} </h2>
         <p>{props.monsters.email}</p>
     </div>
 }
